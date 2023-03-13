5cea477ce1d253c78b0811bcc296aba1d7ae0d84
LinhBQ <linhstartwork@gmail.com>
2023-03-13 21:57:29 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LeftSideComponent } from './components/left-side/left-side.component';
+import { RightSideComponent } from './components/right-side/right-side.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    MainContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
